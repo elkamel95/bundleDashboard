ba083d372365e7c2b5163f96fab987c051610c6e
Elkamel Fahmi <57778698+elkamel95@users.noreply.github.com>
2020-07-22 02:47:04 +0200

    update-Bundle

--- src/Controller/WidgetCustomController.php
+++ src/Controller/WidgetCustomController.php
@@ -5,11 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\WidgetRepository;
+use App\Entity\Widget;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+
 class WidgetCustomController extends AbstractController
 {
-
+  protected $entityClass = Widget::class;
    /**
      * @Route("/api/reset/position/all", name="reset_position",  methods={"PUT"})
      */
@@ -42,8 +50,39 @@ class WidgetCustomController extends AbstractController
       $WidgetService->  updateStatus( $status,$id);
       $serializer = $this->container->get('serializer');
       $WidgetServiceJson = $serializer->serialize($WidgetService, 'json');
+      
       return new Response($WidgetServiceJson, Response::HTTP_OK, ['content-type' => 'application/json']);   
   
+    }
+    /**
+     * @Route("/api/widgets", name="widget_UpdateAll",  methods={"PUT"})
+     */
+    public function updateAllWidget(Request $request,WidgetRepository $widgetService){
+      $serializer = $this->container->get('serializer');
+      $data = json_decode($request->getContent(), true);
+      $entityManager = $this->getDoctrine()->getManager();
+
+
+foreach ($data as $widgets)
+{$widget =  $widgetService->find($widgets["id"]);
+$widget->setBackgroundColor($widgets["backgroundColor"]);
+$widget->setBackgroundSmallWidget($widgets["backgroundSmallWidget"]);
+$widget->setColorSmallWidget($widgets["colorSmallWidget"]);
+$widget->setFont($widgets["font"]);
+$widget->setHeight($widgets["height"]);
+$widget->setTextColor($widgets["textColor"]);
+$widget->setPositionLeft($widgets["positionLeft"]);
+$widget->setPositionRight($widgets["positionRight"]);
+$widget->setUpdateAt($widgets["updateAt"]);
+$widget->setWidth($widgets["width"]);
+$widget->setSize($widgets["size"]);
+$entityManager->flush();
+}
+
+
+
+  return new Response( $request->get('widget'), Response::HTTP_OK, ['content-type' => 'application/json']);   
+
     }
     /**
      * @Route("/api/xml/read", name="xml_readFile",  methods={"GET"})
@@ -58,6 +97,7 @@ class WidgetCustomController extends AbstractController
 foreach ($crawler as $domElement) {
 
 }
+
 return new Response($document->saveXML(), 200, ['Content-Type' => 'text/xml']);
 
   }
